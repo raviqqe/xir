0ad738a6bb254befdfd09adccaa9ee56449fcc86
Yota Toyama <raviqqe@gmail.com>
2024-12-31 18:48:05 +0900

    Fix

--- xir/src/block.rs
+++ xir/src/block.rs
@@ -7,18 +7,18 @@ pub struct Block<A: Allocator> {
     operations: LinkedList<Operation, A>,
 }
 
-impl<'c> Block<'c> {
+impl Block {
     /// Creates a block.
     pub fn new(operations: LinkedList<Operation>) -> Self {
         Self { operations }
     }
 
     /// Creates a block.
-    pub fn operations(&self) -> &LinkedList<'c, Operation> {
+    pub fn operations(&self) -> &LinkedList<Operation> {
         &self.operations
     }
 
-    pub fn operations_mut(&mut self) -> &mut LinkedList<'c, Operation> {
+    pub fn operations_mut(&mut self) -> &mut LinkedList<Operation> {
         &mut self.operations
     }
 }
