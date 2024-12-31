3841b4fd40982b291698e57e96c8ecf6d81e25a0
Yota Toyama <raviqqe@gmail.com>
2024-12-31 19:08:12 +0900

    Fix

--- xir/src/context.rs
+++ xir/src/context.rs
@@ -1,11 +1,13 @@
 use bumpalo::Bump;
 use core::alloc::Allocator;
 
+/// A context.
 pub struct Context {
     allocator: Bump,
 }
 
 impl Context {
+    /// Creates a context.
     pub fn new() -> Self {
         Self {
             allocator: Bump::new(),
