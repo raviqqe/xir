f00af2e2ae7deab716c7c8744b63754a3bd7cc4d
Yota Toyama <raviqqe@gmail.com>
2024-12-31 18:43:07 +0900

    Fix

--- xir/src/block.rs
+++ xir/src/block.rs
@@ -2,12 +2,21 @@ use alloc::collections::LinkedList;
 
 /// A block.
 pub struct Block<'c> {
-    operations: LinkedList<'a>,
+    operations: LinkedList<'a, Operation>,
 }
 
 impl Block {
     /// Creates a block.
     pub fn new() -> Self {
+        Self { operations }
+    }
+
+    /// Creates a block.
+    pub fn operations(&self) -> Self {
+        self.operations
+    }
+
+    pub fn operations_mut(&mut self) -> &mut LinkedList<Operation> {
         Self { operations }
     }
 }
--- xir/src/lib.rs
+++ xir/src/lib.rs
@@ -1,5 +1,7 @@
 //! Blah-blah intermediate representation.
 
+mod block;
 mod operation;
 
+pub use block::*;
 pub use operation::*;
