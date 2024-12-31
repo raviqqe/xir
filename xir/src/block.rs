0de9225fb6fc5eb7340d6af48b25356832e9ea2a
Yota Toyama <raviqqe@gmail.com>
2024-12-31 19:51:16 +0900

    Fix

--- xir/src/block.rs
+++ xir/src/block.rs
@@ -5,13 +5,22 @@ use core::alloc::Allocator;
 /// A block.
 #[derive(Debug)]
 pub struct Block<O: Operation<A>, A: Allocator> {
+    arguments: Vec<O::Type, A>,
     operations: LinkedList<O, A>,
 }
 
 impl<O: Operation<A>, A: Allocator> Block<O, A> {
     /// Creates a block.
-    pub const fn new(operations: LinkedList<O, A>) -> Self {
-        Self { operations }
+    pub const fn new(arguments: Vec<O::Type, A>, operations: LinkedList<O, A>) -> Self {
+        Self {
+            arguments,
+            operations,
+        }
+    }
+
+    /// Returns arguments.
+    pub fn arguments(&self) -> &[O::Type] {
+        &self.arguments
     }
 
     /// Returns a reference to operations.
--- xir/src/lib.rs
+++ xir/src/lib.rs
@@ -7,7 +7,9 @@ extern crate alloc;
 mod block;
 mod context;
 mod operation;
+mod r#type;
 
 pub use block::*;
 pub use context::*;
 pub use operation::*;
+pub use r#type::*;
