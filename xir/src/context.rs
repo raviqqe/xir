2c2c786ef31a3428c35f64c75011aee803339589
Yota Toyama <raviqqe@gmail.com>
2024-12-31 19:06:59 +0900

    Fix

--- xir/src/context.rs
+++ xir/src/context.rs
@@ -1,1 +1,18 @@
-pub struct Context { }
+use bumpalo::Bump;
+use core::alloc::Allocator;
+
+pub struct Context {
+    allocator: Bump,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Self {
+            allocator: Bump::new(),
+        }
+    }
+
+    pub fn allocator(&self) -> impl Allocator {
+        &mut self.allocator
+    }
+}
