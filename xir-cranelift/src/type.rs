75be933dbf087580c9d73b94e7658130460a82fb
Yota Toyama <raviqqe@gmail.com>
2025-01-02 00:21:30 +0900

    Fix

--- xir-cranelift/src/type.rs
+++ xir-cranelift/src/type.rs
@@ -4,3 +4,8 @@ use xir::{Span, Type};
 pub const fn i64_type(span: Span<'_>) -> Type<'_> {
     Type::new("cl.i64", span)
 }
+
+/// Creates a pointer type.
+pub const fn pointer_type(span: Span<'_>) -> Type<'_> {
+    Type::new("cl.pointer", span)
+}
