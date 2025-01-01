ff1c7110aa45321e87c196253e3dea443e8a513a
Yota Toyama <raviqqe@gmail.com>
2025-01-02 00:20:26 +0900

    Fix

--- xir-cranelift/src/lib.rs
+++ xir-cranelift/src/lib.rs
@@ -14,11 +14,10 @@ mod tests {
     use xir::{Block, BlockArgument, Context, Span};
 
     #[test]
-    fn test() {
+    fn add_integers() {
         let context = Context::new();
         let span = Span::new(&context, "", 0, 0);
         let r#type = i64_type(span);
-        let r#type = pointer_type(span);
         let mut block = Block::new(&context, [BlockArgument::new(r#type, span)]);
         let argument = block.arguments()[0].into();
 
@@ -29,7 +28,39 @@ mod tests {
 
         block
             .operations_mut()
-            .push_back(load(&context, value, argument, r#type, span));
+            .push_back(iadd(&context, value, argument, r#type, span));
+    }
+
+    #[test]
+    fn load_integer() {
+        let context = Context::new();
+        let span = Span::new(&context, "", 0, 0);
+        let r#type = i64_type(span);
+        let mut block = Block::new(&context, [BlockArgument::new(r#type, span)]);
+        let argument = block.arguments()[0].into();
+
+        block
+            .operations_mut()
+            .push_back(iconst(&context, r#type, span));
+        let value = block.operations().back().unwrap().value(&context, 0);
+
+        block
+            .operations_mut()
+            .push_back(iadd(&context, value, argument, r#type, span));
+    }
+
+    #[test]
+    fn store_integer() {
+        let context = Context::new();
+        let span = Span::new(&context, "", 0, 0);
+        let r#type = i64_type(span);
+        let mut block = Block::new(&context, [BlockArgument::new(r#type, span)]);
+        let argument = block.arguments()[0].into();
+
+        block
+            .operations_mut()
+            .push_back(iconst(&context, r#type, span));
+        let value = block.operations().back().unwrap().value(&context, 0);
 
         block
             .operations_mut()
