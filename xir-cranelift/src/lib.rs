b3f1c7635c346fbb807140da2f0882211d3d1dbd
Yota Toyama <raviqqe@gmail.com>
2025-01-02 00:27:13 +0900

    Fix

--- xir-cranelift/src/lib.rs
+++ xir-cranelift/src/lib.rs
@@ -49,17 +49,20 @@ mod tests {
     fn store_integer() {
         let context = Context::new();
         let span = Span::new(&context, "", 0, 0);
-        let r#type = i64_type(span);
-        let mut block = Block::new(&context, [BlockArgument::new(r#type, span)]);
-        let argument = block.arguments()[0].into();
+        let integer_type = i64_type(span);
+        let pointer_type = super::pointer_type(span);
+        let mut block = Block::new(
+            &context,
+            [
+                BlockArgument::new(integer_type, span),
+                BlockArgument::new(pointer_type, span),
+            ],
+        );
+        let value = block.arguments()[0].into();
+        let pointer = block.arguments()[1].into();
 
         block
             .operations_mut()
-            .push_back(iconst(&context, r#type, span));
-        let value = block.operations().back().unwrap().value(&context, 0);
-
-        block
-            .operations_mut()
-            .push_back(iadd(&context, value, argument, r#type, span));
+            .push_back(store(&context, value, pointer, span));
     }
 }
