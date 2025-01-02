3f258db2d9dabec3dd35ef68057e519c4a31f88d
Yota Toyama <raviqqe@gmail.com>
2025-01-02 18:57:02 +0900

    scf (#37)

--- xir-cranelift/src/operation.rs
+++ xir-cranelift/src/operation.rs
@@ -3,7 +3,7 @@ use xir::{Context, Operation, Span, Type, Value};
 /// Creates a `iconst` operation.
 pub fn iconst<'a>(context: &'a Context<'a>, r#type: Type<'a>, span: Span<'a>) -> Operation<'a> {
     // TODO Set a `value` attribute.
-    Operation::new(context, "cl.const", [], [r#type], span)
+    Operation::new(context, "cl.const", &[], &[r#type], [], span)
 }
 
 /// Creates an `iadd` operation.
@@ -14,7 +14,7 @@ pub fn iadd<'a>(
     r#type: Type<'a>,
     span: Span<'a>,
 ) -> Operation<'a> {
-    Operation::new(context, "cl.iadd", [lhs, rhs], [r#type], span)
+    Operation::new(context, "cl.iadd", &[lhs, rhs], &[r#type], [], span)
 }
 
 /// Creates a `load` operation.
@@ -24,7 +24,7 @@ pub fn load<'a>(
     pointer: Value<'a>,
     span: Span<'a>,
 ) -> Operation<'a> {
-    Operation::new(context, "cl.load", [pointer], [r#type], span)
+    Operation::new(context, "cl.load", &[pointer], &[r#type], [], span)
 }
 
 /// Creates a `store` operation.
@@ -34,5 +34,5 @@ pub fn store<'a>(
     pointer: Value<'a>,
     span: Span<'a>,
 ) -> Operation<'a> {
-    Operation::new(context, "cl.store", [value, pointer], [], span)
+    Operation::new(context, "cl.store", &[value, pointer], &[], [], span)
 }
---
+++ xir-scf/src/lib.rs
@@ -0,0 +1,24 @@
+//! Simple control flow operations.
+
+use xir::{Block, Context, Operation, Span, Type};
+
+/// An `if` operation.
+pub fn r#if<'a>(context: &'a Context<'a>, types: &[Type<'a>], span: Span<'a>) -> Operation<'a> {
+    Operation::new(context, "scf.if", &[], types, [], span)
+}
+
+/// A `while` operation.
+pub fn r#while<'a>(
+    context: &'a Context<'a>,
+    types: &[Type<'a>],
+    before: Block<'a>,
+    after: Block<'a>,
+    span: Span<'a>,
+) -> Operation<'a> {
+    Operation::new(context, "scf.while", &[], types, [before, after], span)
+}
+
+/// A `yield` operation.
+pub fn r#yield<'a>(context: &'a Context<'a>, types: &[Type<'a>], span: Span<'a>) -> Operation<'a> {
+    Operation::new(context, "scf.while", &[], types, [], span)
+}
