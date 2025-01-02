4bf27bf9d5baa99cbe90b0703c39723ed221179c
Yota Toyama <raviqqe@gmail.com>
2025-01-02 17:35:01 +0900

    Fix

--- xir-scf/src/lib.rs
+++ xir-scf/src/lib.rs
@@ -1,11 +1,19 @@
 //! Simple control flow operations.
 
+use xir::Block;
+
 /// An `if` operation.
 pub fn r#if<'a>(context: &'a Context<'a>, r#type: Type<'a>, span: Span<'a>) -> Operation<'a> {
     Operation::new(context, "cl.const", [], [r#type], span)
 }
 
 /// A `while` operation.
-pub fn r#while<'a>(context: &'a Context<'a>, r#type: Type<'a>, span: Span<'a>) -> Operation<'a> {
+pub fn r#while<'a>(
+    context: &'a Context<'a>,
+    r#type: Type<'a>,
+    before: Block<'a>,
+    after: Block<'a>,
+    span: Span<'a>,
+) -> Operation<'a> {
     Operation::new(context, "cl.const", [], [r#type], span)
 }
