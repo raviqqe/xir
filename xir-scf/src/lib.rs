be87a1c6b269bdb0f5bf9ecf23345a6639c62c6e
Yota Toyama <raviqqe@gmail.com>
2025-01-02 17:37:39 +0900

    Fix

--- xir-scf/src/lib.rs
+++ xir-scf/src/lib.rs
@@ -3,12 +3,23 @@
 use xir::Block;
 
 /// An `if` operation.
-pub fn r#if<'a>(context: &'a Context<'a>, r#type: Type<'a>, span: Span<'a>) -> Operation<'a> {
-    Operation::new(context, "scf.const", [], [r#type], span)
+pub fn r#if<'a>(context: &'a Context<'a>, types: [Type<'a>], span: Span<'a>) -> Operation<'a> {
+    Operation::new(context, "scf.if", [], types, span)
 }
 
 /// A `while` operation.
 pub fn r#while<'a>(
+    context: &'a Context<'a>,
+    r#type: Type<'a>,
+    before: Block<'a>,
+    after: Block<'a>,
+    span: Span<'a>,
+) -> Operation<'a> {
+    Operation::new(context, "scf.while", [], [r#type], span)
+}
+
+/// A `yield` operation.
+pub fn r#yield<'a>(
     context: &'a Context<'a>,
     r#type: Type<'a>,
     before: Block<'a>,
