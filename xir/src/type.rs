8ba8f0f28729aa2389a847bf146832063234fb57
Yota Toyama <raviqqe@gmail.com>
2025-01-11 16:38:02 +0900

    Fix

--- xir/src/type.rs
+++ xir/src/type.rs
@@ -1,22 +1,28 @@
-use crate::Span;
+use crate::{Span, Symbol};
 
 /// A type.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Type<'a> {
+    body: TypeBody<'a>,
     span: Span<'a>,
 }
 
 /// A type body.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum TypeBody<'a> {
-    Named(Symbol<'a>, &'a Type<'a>, Span<'a>),
+    Bool,
+    Function(Symbol<'a>, &'a Type<'a>),
+    I16,
+    I32,
     I64,
+    Named(Symbol<'a>, &'a Type<'a>),
+    Tuple(Symbol<'a>, &'a Type<'a>),
 }
 
 impl<'a> Type<'a> {
     /// Creates a type.
-    pub const fn new(id: &'static str, span: Span<'a>) -> Self {
-        Self { id, span }
+    pub const fn new(body: TypeBody<'a>, span: Span<'a>) -> Self {
+        Self { body, span }
     }
 
     /// Returns an ID.
